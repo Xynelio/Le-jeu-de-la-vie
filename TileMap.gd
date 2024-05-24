b2d5a83ea53bf81c8a94043c74dd24c8c896ab12
laurine-v <118911357+laurine-v@users.noreply.github.com>
2024-05-24 21:33:31 +0200

    fix bugs

--- TileMap.gd
+++ TileMap.gd
@@ -11,6 +11,8 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	#self.set_cell(0,Vector2(1,2),1,Vector2i(0,0))
+	var cells_to_erase = []
+	var cells_to_set = []
 	for x in range(int(get_viewport().size.x / scale.x)):
 		for y in range(int(get_viewport().size.y / scale.y)):
 			var nb_living_cells = 0
@@ -19,8 +21,13 @@ func _physics_process(delta):
 					nb_living_cells += 1
 			if get_cell_source_id(0, Vector2i(x, y)) == -1: # if cell is dead
 				if nb_living_cells == 3:
-					self.set_cell(0,Vector2i(x,y),1,Vector2i(0,0))
+					cells_to_set.append(Vector2i(x,y))
 			else: # cell is alive
 				if nb_living_cells in [0, 1, 4, 5, 6, 7, 8]:
-					self.erase_cell(0, Vector2i(x,y))
+					cells_to_erase.append((Vector2i(x,y)))
+	for cell in cells_to_erase:
+		self.erase_cell(0, cell)
+	for cell in cells_to_set:
+		self.set_cell(0,cell,1,Vector2i(0,0))
+
 		
